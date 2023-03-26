eb13db464133ef7954b50bbe53ff26176fae9fce
RyanNg <ryann@DESKTOP-4N8KPJI>
2023-03-27 05:42:18 +0800

    chore: rename

---
+++ src/main/java/ContentData/AcademicTranscript.java
@@ -0,0 +1,59 @@
+package ContentData;
+
+import java.io.Serializable;
+
+public class AcademicTranscript implements Serializable {
+    private String moduleTaken;
+    transient private Double creditHours;
+    private String moduleResult;
+    private String moduleGPA;
+    private String cgpa;
+
+    public AcademicTranscript(String moduleTaken, Double creditHours, String moduleResult, String moduleGPA, String cgpa) {
+        this.moduleTaken = moduleTaken;
+        this.creditHours = creditHours;
+        this.moduleResult = moduleResult;
+        this.moduleGPA = moduleGPA;
+        this.cgpa = cgpa;
+    }
+
+
+    public AcademicTranscript(){
+        moduleTaken = "BCD";
+        creditHours = 40.0;
+        moduleResult = "A";
+        moduleGPA = "4.0";
+        cgpa = "4.0";
+    }
+
+    public String getModule() {
+        return moduleTaken;
+    }
+
+    public double getCredit() {
+        return creditHours;
+    }
+
+    public String getResult() {
+        return moduleResult;
+    }
+
+    public String getGrade() {
+        return moduleGPA;
+    }
+
+    public String getCgpa() {
+        return cgpa;
+    }
+
+    @Override
+    public String toString() {
+        return "\n==------------== Academic Transcript ==------------==" +
+                "\nModule: '" + moduleTaken + '\'' +
+                "\nCredit Hours: " + creditHours +
+                "\nModule Result: '" + moduleResult + '\'' +
+                "\nModule GPA: '" + moduleGPA + '\'' +
+                "\nCGPA: '" + cgpa + '\'' +
+                "\n";
+    }
+}
